d408e925762496486477c01b8447b6b544d7e689
shashirajhatkemange428-glitch <shashiraj.hatkemange.428@gmail.com>
2025-12-18 19:16:51 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hatke Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shashiraj Hatke Manager'),
+      ),
+      body: const Center(
+        child: Text(
+          'App Started Successfully',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
